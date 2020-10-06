fb288822f669b81dd095eba0c6f1402021d2cbee
Davide Rizzo <sorcio@gmail.com>
2020-10-06 20:55:29 +0200

    scanner

---
+++ pyjson/scanner.py
@@ -0,0 +1,253 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class TokenType(Enum):
+    # Single-character tokens.
+    LEFT_SQUARE_BRACKET = auto()
+    RIGHT_SQUARE_BRACKET = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    COLON = auto()
+    QUOTES = auto()
+
+    # Literals.
+    STRING = auto()
+    NUMBER = auto()
+
+    # Keywords.
+    NULL = auto()
+    FALSE = auto()
+    TRUE = auto()
+    NAN = auto()
+    INFINITY = auto()
+    MINUS_INFINITY = auto()
+
+    # Unparsable.
+    GARBAGE = auto()
+
+    EOF = auto()
+
+
+KEYWORDS = {
+    "null": TokenType.NULL,
+    "false": TokenType.FALSE,
+    "true": TokenType.TRUE,
+    "NaN": TokenType.NAN,
+    "Infinity": TokenType.INFINITY,
+}
+
+SIGNED_KEYWORDS = {
+    "+": {
+        "NaN": TokenType.NAN,
+        "Infinity": TokenType.INFINITY,
+    },
+    "-": {
+        "NaN": TokenType.NAN,
+        "Infinity": TokenType.MINUS_INFINITY,
+    }
+}
+
+CHAR_TOKENS = {
+    "[": TokenType.LEFT_SQUARE_BRACKET,
+    "]": TokenType.RIGHT_SQUARE_BRACKET,
+    "{": TokenType.LEFT_BRACE,
+    "}": TokenType.RIGHT_BRACE,
+    ",": TokenType.COMMA,
+    ":": TokenType.COLON,
+    '"': TokenType.QUOTES,
+}
+
+
+@dataclass(frozen=True)
+class Token:
+    type: TokenType
+    lexeme: str
+    literal: object
+    start: int
+    line: int
+    column: int
+
+
+class Scanner:
+    def __init__(self, source):
+        self.source = source
+        self.start = 0
+        self.current = 0
+        self.line = 1
+        self.line_start = 0
+        self.garbage = False
+
+    def next(self):
+        while True:
+            token = self._scan_token()
+            if token.type == TokenType.GARBAGE:
+                if self.garbage:
+                    # absorb sequences of garbage into a single token
+                    continue
+                self.garbage = True
+            else:
+                self.garbage = False
+            break
+        return token
+
+    def _is_at_end(self):
+        return self.current >= len(self.source)
+
+    def _scan_token(self):
+        while True:
+            self.start = self.current
+            if self._is_at_end():
+                return self._new_token(TokenType.EOF)
+            c = self._advance()
+            if c in CHAR_TOKENS:
+                return self._new_token(CHAR_TOKENS[c])
+            elif c in "+-":
+                if self._peek().isdigit():
+                    return self._number()
+                elif self._peek().isalpha():
+                    return self._keyword(c)
+                else:
+                    return self._new_token(TokenType.GARBAGE)
+            elif c == "/" and self._match("/"):
+                self._comment()
+            elif c in " \r\t":
+                pass
+            elif c == "\n":
+                self.line += 1
+                self.line_start = self.current
+            elif c.isdigit():
+                return self._number()
+            elif c.isalpha():
+                return self._keyword()
+            else:
+                return self._new_token(TokenType.GARBAGE)
+
+    @property
+    def column(self):
+        return self.current - self.line_start
+
+    def _advance(self):
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def _new_token(self, type, literal=None):
+        text = self.source[self.start : self.current]
+        return Token(type, text, literal, self.start, self.line, self.column)
+
+    def _match(self, expected):
+        if self._peek() == expected:
+            self._advance()
+            return True
+        else:
+            return False
+
+    def _peek(self, lookahead=1):
+        pos = self.current + lookahead - 1
+        if pos >= len(self.source):
+            return "\0"
+        return self.source[pos]
+
+    def _number(self):
+        while self._peek().isdigit():
+            self._advance()
+
+        if self._peek() == "." and self._peek(2).isdigit():
+            # consume separator
+            self._advance()
+            while self._peek().isdigit():
+                self._advance()
+
+        if self._peek() in "eE" and self._peek(2) in "-+1234567890":
+            # consume e
+            self._advance()
+            while self._peek() in "-+1234567890":
+                self._advance()
+
+        value = float(self.source[self.start : self.current])
+        return self._new_token(TokenType.NUMBER, value)
+
+    def _keyword(self, sign=""):
+        while self._peek().isalpha():
+            self._advance()
+
+        name = self.source[self.start + len(sign) : self.current]
+        if sign:
+            keywords = SIGNED_KEYWORDS[sign]
+        else:
+            keywords = KEYWORDS
+        try:
+            type = keywords[name]
+        except KeyError:
+            type = TokenType.GARBAGE
+        return self._new_token(type)
+
+    def _comment(self):
+        while self._peek() != "\n" and not self._is_at_end():
+            self._advance()
+
+
+class StringScanner:
+    def __init__(self, source, start=0, line=1):
+        self.source = source
+        self.chars = []
+        self.current = start
+        self.start = start
+        self.line = line
+
+    def scan_string(self):
+        try:
+            while not self._scan_char():
+                pass
+        except IndexError:
+            self._error("Unexpected end of stream while processing string")
+        text = self.source[self.start - 1 : self.current]
+        value = "".join(self.chars)
+        token = Token(TokenType.STRING, text, value, self.start, self.line, self.start)
+        return token
+
+    def _is_at_end(self):
+        return self.current >= len(self.source)
+
+    def _scan_char(self):
+        c = self._advance()
+        if c == "\\":
+            c = self._advance()
+            if c in '"\\/':
+                self.chars.append(c)
+            elif c == "b":
+                self.chars.append("\b")
+            elif c == "f":
+                self.chars.append("\f")
+            elif c == "n":
+                self.chars.append("\n")
+            elif c == "r":
+                self.chars.append("\r")
+            elif c == "t":
+                self.chars.append("\t")
+            elif c == "u":
+                hexcode = "".join(self._advance() for _ in range(4))
+                self.chars.append(chr(int(hexcode, 16)))
+            else:
+                self.chars.append(c)
+        elif c == "\n":
+            self._error("Unexpected line end while processing string")
+        elif c == '"':
+            return True
+        else:
+            self.chars.append(c)
+        return False
+
+    def _advance(self):
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def _error(self, message):
+        raise StringScanError(message, self.start)
+
+
+class StringScanError(Exception):
+    def __init__(self, message, restart_position):
+        super().__init__(message)
+        self.restart_position = restart_position
---
+++ pyjson/tests/test_scanner.py
@@ -0,0 +1,149 @@
+import pytest
+
+from pyjson.scanner import Scanner, TokenType, StringScanner, StringScanError
+
+
+numbers_to_test = [
+    ("0", 0),
+    ("0.0", 0.0),
+    ("1", 1),
+    ("1.0", 1.0),
+    ("1.123456", 1.123456),
+    ("1.02e6", 1.02e6),
+    ("1e-2", 1e-2),
+    ("1.1e-2", 1.1e-2),
+    ("1e308", 1e308),
+    ("1E-2", 1e-2),
+    ("1.1E-2", 1.1e-2),
+    ("1E308", 1e308),
+]
+
+tokens_to_test = [
+    (
+        '[ ] { } , : " null false true NaN Infinity +Infinity -Infinity',
+        """LEFT_SQUARE_BRACKET RIGHT_SQUARE_BRACKET LEFT_BRACE
+        RIGHT_BRACE COMMA COLON QUOTES NULL FALSE
+        TRUE NAN INFINITY INFINITY MINUS_INFINITY""",
+    ),
+    (
+        """
+        [] // foo
+        {} // bar
+        """,
+        "LEFT_SQUARE_BRACKET RIGHT_SQUARE_BRACKET LEFT_BRACE RIGHT_BRACE",
+    ),
+    ('garbage { foo "', "GARBAGE LEFT_BRACE GARBAGE QUOTES"),
+    ('garbage trash poop { foo "', "GARBAGE LEFT_BRACE GARBAGE QUOTES"),
+    ("2ef", "NUMBER GARBAGE"),
+    ("2e3f", "NUMBER GARBAGE"),
+    (
+        """
+        {
+            "hello": {
+                "a": 123,
+                "b": "x",
+            },
+            "foo": ["aa", "bbb", +Infinity, -Infinity, NaN],
+            "x": true,
+            "y": false,
+            "z": null
+        }
+        """,
+        """
+        LEFT_BRACE
+        QUOTES GARBAGE QUOTES COLON LEFT_BRACE
+        QUOTES GARBAGE QUOTES COLON NUMBER COMMA
+        QUOTES GARBAGE QUOTES COLON QUOTES GARBAGE QUOTES COMMA
+        RIGHT_BRACE COMMA
+        QUOTES GARBAGE QUOTES COLON LEFT_SQUARE_BRACKET
+            QUOTES GARBAGE QUOTES COMMA
+            QUOTES GARBAGE QUOTES COMMA
+            INFINITY COMMA
+            MINUS_INFINITY COMMA
+            NAN
+            RIGHT_SQUARE_BRACKET COMMA
+        QUOTES GARBAGE QUOTES COLON TRUE COMMA
+        QUOTES GARBAGE QUOTES COLON FALSE COMMA
+        QUOTES GARBAGE QUOTES COLON NULL
+        RIGHT_BRACE
+        """,
+    ),
+]
+
+strings_to_test = [
+    ('foo"', "foo"),
+    ('"', ""),
+    ('hello world"', "hello world"),
+    (r'\b"', "\b"),
+    (r'\f"', "\f"),
+    (r'\n"', "\n"),
+    (r'\r"', "\r"),
+    (r'\t"', "\t"),
+    (r'hello \n world"', "hello \n world"),
+    (r'\u2603"', "\u2603"),
+    (r'aa\u2603"', "aa\u2603"),
+    (r'\u2603bb"', "\u2603bb"),
+]
+
+failing_strings = [
+    "", "foo", "foo\n\""
+]
+
+
+@pytest.fixture
+def expected_tokens(expected_stream):
+    return [getattr(TokenType, type) for type in expected_stream.split()]
+
+
+@pytest.fixture
+def scanner(source):
+    scanner = Scanner(source)
+    yield scanner
+
+
+@pytest.fixture
+def string_scanner(source):
+    scanner = StringScanner(source)
+    yield scanner
+
+
+@pytest.mark.parametrize("source,expected_stream", tokens_to_test)
+class TestScannerTokens:
+    def test_parse(self, scanner, expected_stream):
+        scanner.next()
+
+    def test_parse_stream(self, scanner, expected_stream):
+        token = None
+        while not token or token.type == TokenType.EOF:
+            token = scanner.next()
+
+    def test_scanner(self, scanner, expected_tokens):
+        stream = []
+        while True:
+            stream.append(scanner.next())
+            if stream[-1].type == TokenType.EOF:
+                break
+        *tokens, eof = stream
+        assert eof.type == TokenType.EOF
+        assert [token.type for token in tokens] == expected_tokens
+
+
+@pytest.mark.parametrize("source,expected", numbers_to_test)
+def test_scan_numbers(scanner, expected):
+    token = scanner.next()
+    assert token.type == TokenType.NUMBER
+    assert token.literal == expected
+
+
+@pytest.mark.parametrize("source,expected", strings_to_test)
+def test_string_scanner(string_scanner, expected):
+    token = string_scanner.scan_string()
+    assert token.type == TokenType.STRING
+    assert token.literal == expected
+
+
+@pytest.mark.parametrize("source", failing_strings)
+def test_string_scanner_error(string_scanner):
+    with pytest.raises(StringScanError) as error:
+        string_scanner.scan_string()
+    assert error.value.restart_position == 0
